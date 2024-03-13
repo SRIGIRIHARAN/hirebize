025193b7f7bf06aea9957a0bb49df39ba2f30313
SRIGIRIHARAN <giriharan0102@gmail.com>
2024-03-13 08:02:09 +0530

    Second commit

--- src/pages/Home/components/SecFive.js
+++ src/pages/Home/components/SecFive.js
@@ -3,21 +3,25 @@ import { Button, Card, Col, Container, Row } from 'react-bootstrap'
 
 const SecFive = () => {
   return (
-    <Container>
-        <Row xs={1} md={2} lg={3} className="g-4">
-            {Array.from({ length: 6 }).map((_, idx) => (
-                <Col key={idx}>
-                    <Card>
-                        <Card.Img variant="top" src="./assets/case-study-1.png" />
-                        <Card.Body>
-                            <Button variant="primary">Go somewhere</Button>
-                            <Card.Title>Workflow System Energy</Card.Title>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            ))}
-        </Row>
-    </Container>
+    <div className="s-five-banner">
+        
+        <Container>
+            <h1>Case Study</h1>
+            <Row xs={1} md={2} lg={3} className="g-4" style={{border: '1px solid red'}}>
+                {Array.from({ length: 6 }).map((_, idx) => (
+                    <Col key={idx}>
+                        <Card>
+                            <Card.Img variant="top" src="./assets/case-study-1.png" />
+                            <Card.Body>
+                                <Button variant="primary">Go somewhere</Button>
+                                <Card.Title>Workflow System Energy</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
+    </div>
   )
 }
 
